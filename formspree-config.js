285ae3629db508af07690ee709baa411ef3854d1
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-08-24 23:24:07 +0200

    Add Formspree configuration placeholder

---
+++ formspree-config.js
@@ -0,0 +1,8 @@
+/*
+ * Replace the empty value with the public Formspree form ID.
+ * Example: for https://formspree.io/f/xabcdefg, use "xabcdefg".
+ * Do not add a Formspree API key to this public repository.
+ */
+window.PARENTING_PLAN_CONFIG = {
+  formspreeFormId: ""
+};
